0128ad319785dccefe993752e305273833ec93a8
Roberto Caroca <63612984+RobertoCaroca@users.noreply.github.com>
2023-05-20 19:11:33 -0400

    Refactoring code

--- atm.jsx
+++ atm.jsx
@@ -1,108 +1,88 @@
-const ATMDeposit = ({ isDeposit, onChange, isValid }) => {
-  const choice = ['Deposit', 'Cash Back'];
+const ATMDeposit = ({ onChange, onDeposit, onCashBack, isDeposit }) => {
+  const handleAmountChange = (event) => {
+    const amount = Number(event.target.value);
+    onChange(amount);
+  };
+
+  const handleDeposit = (event) => {
+    event.preventDefault();
+    onDeposit();
+  };
+
+  const handleCashBack = (event) => {
+    event.preventDefault();
+    onCashBack();
+  };
 
   return (
     <div>
-      <h3>{choice[Number(!isDeposit)]}</h3>
-      <input 
-        id="number-input" 
-        type="number" 
-        min="0"
-        onChange={onChange}
-      />
-      {isDeposit && (
-        <input 
-          type="submit" 
-          disabled={!isValid} 
-          value="Submit" 
-          id="submit-input"
-        />
+      <h3>{isDeposit ? "Deposit" : "Cash Back"}</h3>
+      <input type="number" onChange={handleAmountChange} />
+      {isDeposit ? (
+        <button onClick={handleDeposit}>Submit</button>
+      ) : (
+        <button onClick={handleCashBack}>Submit</button>
       )}
     </div>
   );
 };
 
 const Account = () => {
-  const [deposit, setDeposit] = React.useState(0);
-  const [totalState, setTotalState] = React.useState(0);
-  const [isDeposit, setIsDeposit] = React.useState(false);
-  const [validTransaction, setValidTransaction] = React.useState(false);
+  const [balance, setBalance] = React.useState(0);
+  const [amount, setAmount] = React.useState(0);
+  const [isDeposit, setIsDeposit] = React.useState(true);
 
-  const handleChange = (event) => {
-    const value = Number(event.target.value);
-  
-    if (totalState === 0 && value >= 0) {
-      setDeposit(value);
-      setValidTransaction(true);
-    } else if (totalState !== 0) {
-      setDeposit(value);
-      setValidTransaction(value >= 0 && (isDeposit || totalState + value >= 0));
-    }
+  const handleChange = (amount) => {
+    setAmount(amount);
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-  
-    if (isDeposit) {
-      setTotalState(totalState + deposit);
-    } else {
-      setTotalState(totalState - deposit);
-    }
-  
-    setValidTransaction(false);
-    setDeposit(0);
+  const handleDeposit = () => {
+    const newBalance = balance + amount;
+    setBalance(newBalance);
+    setAmount(0);
   };
-  
-  const handleModeSelect = (event) => {
-    const mode = event.target.value;
 
-    setIsDeposit(mode === 'Deposit');
-    setValidTransaction(false);
-    setDeposit(0);
+  const handleCashBack = () => {
+    if (amount > balance) {
+      alert("You don't have enough balance for cash back");
+      return;
+    }
+
+    const newBalance = balance - amount;
+    setBalance(newBalance);
+    setAmount(0);
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h2 id="total">Account Balance $ {totalState} </h2>
-      {totalState === 0 && (
+    <div>
+      <h2>Account Balance: ${balance}</h2>
+      {balance === 0 ? (
+        <ATMDeposit
+          onChange={handleChange}
+          onDeposit={handleDeposit}
+          onCashBack={handleCashBack}
+          isDeposit={isDeposit}
+        />
+      ) : (
         <>
-          <label htmlFor="deposit-input">Enter an amount to deposit:</label>
-          <input
-            id="deposit-input"
-            type="number"
-            min="0"
-            onChange={handleChange}
-          />
-          <input 
-            type="submit" 
-            disabled={!validTransaction} 
-            value="Submit" 
-            id="submit-input"
-          />
-        </>
-      )}
-      {totalState !== 0 && (
-        <>
-          <label htmlFor="mode-select">Select an action below to continue:</label>
-          <select onChange={handleModeSelect} name="mode" id="mode-select">
-            <option value="">-- Choose Option --</option>
+          <label>Select an action:</label>
+          <select
+            onChange={(event) => setIsDeposit(event.target.value === "Deposit")}
+            defaultValue="Deposit"
+          >
             <option value="Deposit">Deposit</option>
-            <option value="Cash Back">Cash Back</option>
+            <option value="CashBack">Cash Back</option>
           </select>
-          {isDeposit !== null && (
-            <ATMDeposit
-              isDeposit={isDeposit}
-              onChange={handleChange}
-              isValid={validTransaction}
-            />
-          )}
+          <ATMDeposit
+            onChange={handleChange}
+            onDeposit={handleDeposit}
+            onCashBack={handleCashBack}
+            isDeposit={isDeposit}
+          />
         </>
       )}
-      {validTransaction === false && (
-        <p className="error-message">Invalid transaction.</p>
-      )}
-    </form>
+    </div>
   );
 };
 
-ReactDOM.render(<Account />, document.getElementById('root'));
+ReactDOM.render(<Account />, document.getElementById("root"));
